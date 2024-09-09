38dffe09990702456596a8a38bed4192c03503ad
maelikax <mikaelanavarro21@gmail.com>
2024-09-09 16:37:34 -0500

    set up project (can make changes)

--- main.py
+++ main.py
@@ -16,17 +16,49 @@ Output:
 # Import modules
 import pygame
 
-# Set up Pygame, initialize size of screen, have users able to open/close game
-pygame.init()
+# Run game
+def runGame():
+    # Set up Pygame, initialize size of screen, have users able to open/close game
+    pygame.init()
 
-screen_width = 800
-screen_height = 600
+    screen_width = 1200
+    screen_height = 600
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+    screen = pygame.display.set_mode((screen_width, screen_height))
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-pygame.quit()
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+# Grid for player to guess other player's location
+def grid1():
+    pass
+
+# Grid for player to place their ships
+def grid2():
+    pass
+
+# Create ships
+def createShips():
+    pass
+
+# Place ships
+def placeShips():
+    pass
+
+# Count hits
+def countHits():
+    pass
+
+# Count misses
+def countMisses():
+    pass
+
+# Blank screen when switching turns
+def endTurn():
+    pass
+
+runGame()
